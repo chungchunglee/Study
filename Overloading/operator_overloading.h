5c5b66c3e45fbd6f55875a7ef9cffe6df7fe5494
chungchunglee <fire6376@gmail.com>
2022-03-02 17:05:19 +0900

    연산자 오버로딩 완료 ::동적할당과 새로운 생성자 추가

--- Overloading/operator_overloading.h
+++ Overloading/operator_overloading.h
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <cctype>
+#include <cstdlib>
+
 class Complex
 {
 private:
 	double real, img;
 
 public:
-	//Complex(double real, double img);
 	Complex(double real, double img) :real(real), img(img) {}
-	//Complex(const Complex& c);
 	Complex(const Complex& c) { real = c.real, img = c.img; }
+	Complex(const char* str);
 
 	Complex operator+(const Complex& c) const;
 	Complex operator-(const Complex& c) const;
@@ -22,6 +24,11 @@ public:
 	Complex& operator/=(const Complex& c);
 
 	Complex operator+(const char* str);
+	Complex operator-(const char* str);
+	Complex operator*(const char* str);
+	Complex operator/(const char* str);
+
+	double get_number(const char* str, const int begin, const int end);
 
 	void display();
 };
