17d7349143c49460a24223a74c608d61dbcd0e57
chungchunglee <fire6376@gmail.com>
2022-03-11 16:59:58 +0900

    알고리즘

--- Algorithm/__study_origin/P7_가장큰수.cpp
+++ Algorithm/__study_origin/P7_가장큰수.cpp
@@ -52,6 +52,41 @@ int main()
 {
     cout << solution({0,0,0,0,0}) << endl;
 }
+#ifdef __OTHERS__
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool compare(const string& a, const string& b)
+{
+    if (b + a < a + b)
+        return true;
+    return false;
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+
+    vector<string> strings;
+
+    for (int i : numbers)
+        strings.push_back(to_string(i));
+
+    sort(strings.begin(), strings.end(), compare);
+
+    for (auto iter = strings.begin(); iter < strings.end(); ++iter)
+        answer += *iter;
+
+    if (answer[0] == '0')
+        answer = "0";
+
+    return answer;
+}
+#endif // __OTHERS__
+
+
 //fail 1
 /*
 string solution(vector<int> numbers) {
